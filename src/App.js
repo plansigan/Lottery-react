2bbd2278773b043dd8cc497cff75d3d614bd7700
plansigan <kemchii.69@gmail.com>
2019-06-11 07:39:29 +0800

    finished project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Lottery from './Lottery'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Lottery/>
+      <Lottery title="Mini Daily" maxNum={10} maxBalls={4}/>
     </div>
   );
 }
